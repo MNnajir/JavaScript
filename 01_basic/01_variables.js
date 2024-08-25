3822f72d48f7782d09f46cc90195d981507aec3c
Najir Ansari <121566044+MNnajir@users.noreply.github.com>
2024-08-25 17:30:34 +0000

    variable cognfi done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountId =12356
+let accountEmail ="najir608@gmail.com"
+var accountPassword ="2345"
+accountCity ="janakpur"
+ let accountState;
+
+//accountId = 2
+console.log(accountId)
+accountEmail="nvnv@hn.com"
+accountPassword="23423"
+accountCity="ktm"
+console.table([accountId ,accountEmail , accountPassword , accountCity ,accountState])
