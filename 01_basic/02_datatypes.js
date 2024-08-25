c46f141344ababfcf46219a4ba443968f25326f2
Najir Ansari <121566044+MNnajir@users.noreply.github.com>
2024-08-25 19:02:26 +0000

    datatype done

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+// alert (3+3) we are using nodejs , not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+    console.log("Najir")
+    let name = "Najir" 
+    let age = 23 
+    let isLoggedIn = false
+    let state;
+    // number =>2 to power 53
+    // bigint
+    // string =>""
+    // boolean => true/false
+    // null => standalone value 
+    // undefined 
+    // symbol => unique
+    // object
+    console.log(typeof undefined);  //undefined
+    console.log(typeof null);  //object
