1ee69a92f606ed57cfbb1e32c8e897506a170740
Najir Ansari <121566044+MNnajir@users.noreply.github.com>
2024-08-26 05:36:06 +0000

    conversionOperationn datatype done

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "33"
+console.log(typeof score)
+console.log(typeof(score))
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abs" => NaN (Not a Number)
+// true => 1 false => 0
+
+let isLoggedIn = "Najir"
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+// 1=> true 0 => false
+// "" => false 
+// "Najir" => true
+
+let someNumber = 33
+let StringNumber = String(someNumber)
+console.log(StringNumber)
+console.log(typeof StringNumber)
