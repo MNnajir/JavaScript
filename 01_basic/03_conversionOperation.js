f8d3f2cca4658ed494531a7a8294e570f85cac30
Najir Ansari <121566044+MNnajir@users.noreply.github.com>
2024-08-26 08:18:51 +0000

    operation done

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -21,3 +21,38 @@ let someNumber = 33
 let StringNumber = String(someNumber)
 console.log(StringNumber)
 console.log(typeof StringNumber)
+
+
+// ************** Operation *******************
+
+
+let value = 3 
+
+let negvalue = -value
+// console.log(negvalue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+// modulus operator (%) returns the remainder of the division of the number before it by the number after it
+
+let str1 = "Hello"
+let str2 = " Najir"
+let str3 = str1  + str2
+ console.log(str3)
+ console.log("1" +2)
+ console.log(1 + "2");
+ console.log("1" + 2 + 2);
+ console.log(1 + 2 + "2" );
+ console.log( (3+4) * 5 % 3)
+
+ console.log(true)
+ console.log(+"")
+
+ let gameCounter = 100
+ gameCounter++
+//  ++gameCounter
+ console.log(gameCounter)
