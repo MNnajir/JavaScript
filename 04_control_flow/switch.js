3bf79602ca4671ffe50079f1fdf89d3fd2962a0d
Najir Ansari <121566044+MNnajir@users.noreply.github.com>
2024-09-03 04:01:54 +0000

    Switch statement done

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,48 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// let month = 3
+// switch (month) {
+//     case 1:
+//         console.log("Janaury")
+//         break;
+//         case 2:
+//             console.log("Feb")
+//             break;
+//             case 3:
+//         console.log("March")
+//         break;
+//         case 2:
+//         console.log("April")
+//         break;
+//     default:
+//         console.log("default case match");
+        
+//         break;
+// }
+
+const month = "march"
+switch (month) {
+    case "jan":
+        console.log("Janaury")
+        break;
+        case "feb":
+            console.log("Feb")
+            break;
+            case "march":
+        console.log("March")
+        break;
+        case "april":
+        console.log("April")
+        break;
+    default:
+        console.log("default case match");
+        
+        break;
+}
