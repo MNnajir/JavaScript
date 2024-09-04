49d648c9e5c7cc10ccedb2d0920168be09b23726
Najir Ansari <121566044+MNnajir@users.noreply.github.com>
2024-09-04 07:30:57 +0000

    forin loop done

---
+++ 05_iterations/four.js
@@ -0,0 +1,24 @@
+// const myObject = {
+//     js: 'javascript',
+//     cpp: 'c++',
+//     rb: "ruby",
+//     swift: "swift by apple"
+// }
+//for (const key in myObject) {
+   // console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+//}
+const programming = ["js", "rb", "py", "jave", "cpp"]
+for (const key in programming) {
+    console.log(programming[key]);
+    
+}
+// const map = new Map()
+// map.set('Np', "Nepal")
+// map.set('USA', "United State Of America")
+// map.set('IN', "India")
+// map.set('Np', "Nepal")
+// for (const key in map) {
+//     console.log(key);
+    
+// }
